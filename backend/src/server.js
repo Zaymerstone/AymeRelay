a19a6d1d7ba9d1ec291f948a47340e49128b437b
Egor Nesterov <egor.nesterov2002@gmail.com>
2026-02-23 10:35:43 +0100

    engines field added in package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,12 +16,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes); // to link all pathes starting with /api/messages to the messageRoutes router logic
 
 // make ready for deployment
+// I do this because of sevalla deployment process where a backend server is now serving the fronted server inside
+// static files sit in frontend/dist, so I need to tell the backend server to serve those files when in production mode, and also to check for the index.html file in that folder for every incoming request, so that the frontend routing can work properly
+// When deployed, also act like a frontend server
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // to serve the static files from the frontend build folder
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // to serve the static files from the frontend/dist folder build folder
 
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html")); // for every incoming request check this folder first, this file first
+  }); // if we get request https://aymerelay.com/chat, without this it would be cannot GET /chat, but its good because we have reference to index and to front here. React router type of vibe
 }
 
 app.listen(PORT, () => {
